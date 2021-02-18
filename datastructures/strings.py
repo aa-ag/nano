026fa35694bf8bf7b76dbd736d2189d665d6d083
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-17 19:14:03 -0600

    Python Strings are Arrays. They are arrays of bytes representing unicode characters.

---
+++ datastructures/strings.py
@@ -0,0 +1,10 @@
+# Exercise 1. Reverse Strings
+
+'''
+ In this first exercise, the goal is to write a function 
+ that takes a string as input and then returns the reversed string.
+'''
+
+string = 'Hello, world!'
+
+print(sorted(string))
