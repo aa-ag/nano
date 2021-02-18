4ffa2a8fb301fde0e58e414d69065d82ccc58f70
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-17 19:15:56 -0600

    exercise 1 completed, test cases

--- datastructures/strings.py
+++ datastructures/strings.py
@@ -5,6 +5,13 @@
  that takes a string as input and then returns the reversed string.
 '''
 
-string = 'Hello, world!'
 
-print(sorted(string))
+def string_reverser(our_string):
+    return our_string[::-1]
+
+
+print("Pass" if ('retaw' == string_reverser('water')) else "Fail")
+print("Pass" if ('!noitalupinam gnirts gnicitcarP' ==
+                 string_reverser('Practicing string manipulation!')) else "Fail")
+print("Pass" if ('3432 :si edoc esuoh ehT' ==
+                 string_reverser('The house code is: 2343')) else "Fail")
