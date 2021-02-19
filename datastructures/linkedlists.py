231f38e3ece6a33159260ef0c3afc36baba92b75
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-18 19:29:34 -0600

    aStep 2

--- datastructures/linkedlists.py
+++ datastructures/linkedlists.py
@@ -19,4 +19,16 @@ head.next = Node(2)
 # then either:
 # print(new_node.value)
 # or
+# print(head.next.value)
+
+# Step 2.  Add three more nodes to the list, with the values `4`, `3`, and `5`
+
+head.next.next = Node(4)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(5)
+
+print(head.value)
 print(head.next.value)
+print(head.next.next.value)
+print(head.next.next.next.value)
+print(head.next.next.next.next.value)
