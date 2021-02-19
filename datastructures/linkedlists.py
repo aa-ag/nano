22549dacc00d7a7cd974daf6827d8c36c1397d78
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-18 19:55:27 -0600

    function to create linked list without less manual

--- datastructures/linkedlists.py
+++ datastructures/linkedlists.py
@@ -12,8 +12,9 @@ head.next.next = Node(4)
 head.next.next.next = Node(3)
 head.next.next.next.next = Node(5)
 
-
 # Exercise 2 -  Traversing the list
+
+
 def print_all_nodes(head):
     current_node = head
 
@@ -22,4 +23,51 @@ def print_all_nodes(head):
         current_node = current_node.next
 
 
-print_all_nodes(head)
+# print_all_nodes(head)
+
+# Creating a Linked List, less manual
+
+
+def create_linked_list(input_list):
+    head = None
+    for value in input_list:
+        if head is None:
+            head = Node(value)
+        else:
+            # Move to the tail (the last node)
+            current_node = head
+            while current_node.next:
+                current_node = current_node.next
+
+            current_node.next = Node(value)
+    return head
+
+
+def test_function(input_list, head):
+    try:
+        if len(input_list) == 0:
+            if head is not None:
+                print("Fail")
+                return
+        for value in input_list:
+            if head.value != value:
+                print("Fail")
+                return
+            else:
+                head = head.next
+        print("Pass")
+    except Exception as e:
+        print("Fail: " + e)
+
+
+input_list = [1, 2, 3, 4, 5, 6]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = [1]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = []
+head = create_linked_list(input_list)
+test_function(input_list, head)
