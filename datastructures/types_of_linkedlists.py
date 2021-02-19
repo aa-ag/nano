0e26a0dd422ae93cdd246691e001f80bce0f260e
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-18 20:24:49 -0600

    doubly linked list

--- datastructures/types_of_linkedlists.py
+++ datastructures/types_of_linkedlists.py
@@ -35,13 +35,56 @@ class LinkedList:
         return out_list
 
 
-linked_list = LinkedList()
-linked_list.append(3)
-linked_list.append(2)
-linked_list.append(-1)
-linked_list.append(0.2)
+# linked_list = LinkedList()
+# linked_list.append(3)
+# linked_list.append(2)
+# linked_list.append(-1)
+# linked_list.append(0.2)
 
-print("Pass" if (linked_list.to_list() == [3, 2, -1, 0.2]) else "Fail")
+# print("Pass" if (linked_list.to_list() == [3, 2, -1, 0.2]) else "Fail")
 
-# (2) doubly-linked lists, and
+
+# (2) doubly-linked lists
+# has connections backwards and forwards through the list.
+class DoubleNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.previous = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = DoubleNode(value)
+            self.tail = self.head
+            return
+
+        self.tail.next = DoubleNode(value)
+        self.tail.next.previous = self.tail
+        self.tail = self.tail.next
+        return
+
+
+linked_list = DoublyLinkedList()
+linked_list.append(1)
+linked_list.append(-2)
+linked_list.append(4)
+
+print("Going forward through the list, should print 1, -2, 4")
+node = linked_list.head
+while node:
+    print(node.value)
+    node = node.next
+
+print("\nGoing backward through the list, should print 4, -2, 1")
+node = linked_list.tail
+while node:
+    print(node.value)
+    node = node.previous
+
 # (3) circular lists.
