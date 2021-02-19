7ae76203782512e15199c7971eaa1efbeebc1670
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-18 19:34:32 -0600

    traversing simple linked list with while loop

--- datastructures/linkedlists.py
+++ datastructures/linkedlists.py
@@ -1,6 +1,4 @@
-# to implement a Linked List
-# first, create a "Node" class
-# since a Linked List is a list of linked nodes
+# implement a Linked List
 
 class Node():
     def __init__(self, value):
@@ -9,26 +7,15 @@ class Node():
 
 
 head = Node(1)
-# print(head.value)
-# print(head.next)
-
-# new_node = Node(2)
-# head.next = new_node
-# alternatively
 head.next = Node(2)
-# then either:
-# print(new_node.value)
-# or
-# print(head.next.value)
-
-# Step 2.  Add three more nodes to the list, with the values `4`, `3`, and `5`
-
 head.next.next = Node(4)
 head.next.next.next = Node(3)
 head.next.next.next.next = Node(5)
 
-print(head.value)
-print(head.next.value)
-print(head.next.next.value)
-print(head.next.next.next.value)
-print(head.next.next.next.next.value)
+# Exercise 2 -  Traversing the list
+
+current_node = head
+
+while current_node is not None:
+    print(current_node.value)
+    current_node = current_node.next
