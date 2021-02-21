f54160687e0a06bb55e1256fe5b2aee5a4aaf987
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-20 19:27:47 -0600

    tests for reversed function

--- datastructures/reverse_linked_list.py
+++ datastructures/reverse_linked_list.py
@@ -1,5 +1,4 @@
 # Helper Code
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -30,10 +29,26 @@ class LinkedList:
     def __repr__(self):
         return str([v for v in self])
 
+# --- reversing linkedlist
 
+
+def reverse(linked_list):
+    """
+     Reverse the inputted linked list
+    """
+
+    # TODO: Write your function to reverse linked lists here
+
+    pass
+
+
+# testing
 llist = LinkedList()
 for value in [4, 2, 5, 1, -3, 0]:
     llist.append(value)
 
-print(llist)
 # [4, 2, 5, 1, -3, 0]
+flipped = reverse(llist)
+is_correct = list(flipped) == list(
+    [0, -3, 1, 5, 2, 4]) and list(llist) == list(reverse(flipped))
+print("Pass" if is_correct else "Fail")
