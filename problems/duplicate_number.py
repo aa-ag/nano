bb15130c92fb94daf3dc19d02f22051fb0cdc026
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-21 19:43:17 -0600

    class code for duplicate_number()

--- problems/duplicate_number.py
+++ problems/duplicate_number.py
@@ -3,7 +3,26 @@ def duplicate_number(arr):
     :param - array containing numbers in the range [0, len(arr) - 2]
     return - the number that is duplicate in the arr
     """
-    return [i for i in arr if arr.count(i) > 1][0]
+
+    # return [i for i in arr if arr.count(i) > 1][0]
+    current_sum = 0
+    expected_sum = 0
+
+    # Traverse the original array in the forward direction
+    for num in arr:
+        current_sum += num
+
+    # Traverse from 0 to (length of array-1) to get the expected_sum
+    # Alternatively, you can use the formula for sum of an Arithmetic Progression to get the expected_sum
+
+    # The argument of range() functions are:
+    # starting index [OPTIONAL], ending index (non exclusive), and the increment/decrement size [OPTIONAL]
+    # It means that if the array length = n, loop will run form 0 to (n-2)
+    for i in range(len(arr) - 1):
+        expected_sum += i
+
+    # The difference between the
+    return current_sum - expected_sum
 
 
 def test_function(test_case):
