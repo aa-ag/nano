5b85c14af3fa3f24e3a78af413955463b7bd6a0d
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-24 19:28:45 -0600

    push() method inside Stack() class

--- datastructures/stacks/stacks.py
+++ datastructures/stacks/stacks.py
@@ -4,6 +4,18 @@ class Stack:
         self.next_index = 0
         self.num_elements = 0
 
+    def push_to_stack(self, data):
+        self.arr[self.next_index] = data
+        self.next_index += 1
+        self.num_elements += 1
+
 
 stack = Stack()
 print(stack.arr)
+# [None, None, None, None, None, None, None, None, None, None]
+
+stack.push_to_stack(1)
+stack.push_to_stack(2)
+stack.push_to_stack(3)
+print(stack.arr)
+# [1, 2, 3, None, None, None, None, None, None, None]
