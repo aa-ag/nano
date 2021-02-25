f3328e48824c9974e9bbce63e831cea6cc2a9bee
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-24 19:44:46 -0600

    handling stack overflow

--- datastructures/stacks/stacks.py
+++ datastructures/stacks/stacks.py
@@ -5,17 +5,28 @@ class Stack:
         self.num_elements = 0
 
     def push_to_stack(self, data):
+        if self.next_index == len(self.arr):
+            print("Stackoverflow! Increasing capacity...")
+            self.handle_stack_capacity()
+
         self.arr[self.next_index] = data
         self.next_index += 1
         self.num_elements += 1
+
+    def handle_stack_capacity(self):
+        old_arr = self.arr
+
+        self.arr = [None for _ in range(2 * len(self.arr))]
+        for i, element in enumerate(old_arr):
+            self.arr[i] = element
 
 
 stack = Stack()
 print(stack.arr)
 # [None, None, None, None, None, None, None, None, None, None]
 
-stack.push_to_stack(1)
-stack.push_to_stack(2)
-stack.push_to_stack(3)
+for _ in range(11):
+    stack.push_to_stack(_)
+
 print(stack.arr)
 # [1, 2, 3, None, None, None, None, None, None, None]
