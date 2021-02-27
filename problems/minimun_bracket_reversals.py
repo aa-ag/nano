c9a9ccc2d819d9468aada2a5640c37273b60701c
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-27 10:51:40 -0600

    class code

--- problems/minimun_bracket_reversals.py
+++ problems/minimun_bracket_reversals.py
@@ -44,43 +44,65 @@ class Stack:
 ###--- FIND MINIMUN BRACKET REVERSAL ---###
 def minimum_bracket_reversals(input_string):
 
+    if len(input_string) % 2 == 1:
+        return -1
+
     stack = Stack()
+    count = 0
+    for bracket in input_string:
+        if stack.is_empty():
+            stack.push(bracket)
+        else:
+            top = stack.top()
+            if top != bracket:
+                if top == '{':
+                    stack.pop()
+                    continue
+            stack.push(bracket)
 
-    for i in input_string:
-        stack.push(i)
+    ls = list()
+    while not stack.is_empty():
+        first = stack.pop()
+        second = stack.pop()
+        ls.append(first)
+        ls.append(second)
+        if first == '}' and second == '}':
+            count += 1
+        elif first == '{' and second == '}':
+            count += 2
+        elif first == '{' and second == '{':
+            count += 1
+    return count
 
-    node = stack.head
-    while node:
-        print(node.data)
-        node = node.next
 
-    print(stack.size())
+# minimum_bracket_reversals("}}}}")  # 2
+# minimum_bracket_reversals("{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}")  # 13
+# minimum_bracket_reversals("}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{")  # 2
+# minimum_bracket_reversals("}}{}{}{}{}{}{}{}{}{}{}{}{}{}{}")  # 1
 
-
-minimum_bracket_reversals("}}}}")
 ###--- TESTING ---###
-# def test_function(test_case):
-#     input_string = test_case[0]
-#     expected_output = test_case[1]
-#     output = minimum_bracket_reversals(input_string)
+def test_function(test_case):
+    input_string = test_case[0]
+    expected_output = test_case[1]
+    output = minimum_bracket_reversals(input_string)
 
-#     if output == expected_output:
-#         print("Pass")
-#     else:
-#         print("Fail")
+    if output == expected_output:
+        print("Pass")
+    else:
+        print("Fail")
 
 
-# test_case_1 = ["}}}}", 2]
-# test_function(test_case_1)
+test_case_1 = ["}}}}", 2]
+test_function(test_case_1)
 
-# test_case_2 = ["}}{{", 2]
-# test_function(test_case_2)
+test_case_2 = ["}}{{", 2]
+test_function(test_case_2)
 
-# test_case_3 = ["{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}", 13]
-# test_function(test_case_1)
+test_case_3 = ["{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}", 13]
+test_function(test_case_1)
 
-# test_case_4 = ["}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{", 2]
-# test_function(test_case_2)
+test_case_4 = ["}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{", 2]
+test_function(test_case_2)
 
-# test_case_5 = ["}}{}{}{}{}{}{}{}{}{}{}{}{}{}{}", 1]
-# test_function(test_case_3)
+test_case_5 = ["}}{}{}{}{}{}{}{}{}{}{}{}{}{}{}", 1]
+test_function(test_case_3)
