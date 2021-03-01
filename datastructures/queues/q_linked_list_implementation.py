acb9e59ce8ba76c9bf3fe826e521351f9ff47d7a
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-28 20:37:33 -0600

    class code

--- datastructures/queues/q_linked_list_implementation.py
+++ datastructures/queues/q_linked_list_implementation.py
@@ -1,8 +1,9 @@
 ###--- CREATE LINKED LIST ---###
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.head = None
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
 ###--- CREATE QUEUE ---###
@@ -13,23 +14,27 @@ class Queue:
         self.tail = None
         self.num_elements = 0
 
-    def enqueue(self, data):
-        new_node = Node(data)
+    def enqueue(self, value):
+        new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = self.head
         else:
+            # add data to the next attribute of the tail (i.e. the end of the queue)
             self.tail.next = new_node
-            self.tail = self.head
+            # shift the tail (i.e., the back of the queue)
+            self.tail = self.tail.next
         self.num_elements += 1
 
-    def dequeue(self, data):
+    def dequeue(self):
         if self.is_empty():
             return None
-        old_head = self.head.data
+        value = self.head.value
+        # copy the value to a local variable
+        # shift the head (i.e., the front of the queue)
         self.head = self.head.next
         self.num_elements -= 1
-        return old_head
+        return value
 
     def size(self):
         return self.num_elements
@@ -39,3 +44,24 @@ class Queue:
 
 
 ###--- TEST ---###
+# Setup
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+
+print("Pass" if (q.dequeue() == 2) else "Fail")
+print("Pass" if (q.dequeue() == 3) else "Fail")
+print("Pass" if (q.dequeue() == 4) else "Fail")
+
+q.enqueue(5)
+print("Pass" if (q.size() == 1) else "Fail")
