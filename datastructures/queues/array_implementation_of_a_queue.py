ce0f0a76e2a1061cbfe99a8928f46704bc600c88
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-28 19:54:53 -0600

    created Node, created Queue

--- datastructures/queues/array_implementation_of_a_queue.py
+++ datastructures/queues/array_implementation_of_a_queue.py
@@ -62,6 +62,22 @@ class Queue:
 
 
 ###--- TESTING __init__ ---###
+# Setup
 q = Queue()
-print(q.arr)
-print("Pass" if q.arr == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] else "Fail")
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print("Pass" if (q.dequeue() == 2) else "Fail")
+print("Pass" if (q.dequeue() == 3) else "Fail")
+print("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print("Pass" if (q.size() == 1) else "Fail")
---
+++ datastructures/queues/q_linked_list_implementation.py
@@ -0,0 +1,16 @@
+###--- CREATE LINKED LIST ---###
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.head = None
+
+
+###--- CREATE QUEUE ---###
+class Queue:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.num_elements = 0
+
+###--- TEST ---###
