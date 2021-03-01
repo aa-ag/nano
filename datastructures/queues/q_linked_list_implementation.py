7a2d7523e71ee113498fb1d1a04690743485e1a7
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-28 20:02:15 -0600

    enqueue()

--- datastructures/queues/q_linked_list_implementation.py
+++ datastructures/queues/q_linked_list_implementation.py
@@ -13,4 +13,14 @@ class Queue:
         self.tail = None
         self.num_elements = 0
 
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next = new_node
+            self.tail = self.head
+        self.num_elements += 1
+
 ###--- TEST ---###
