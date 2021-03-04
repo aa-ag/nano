bbc0e9a2227e3d05ee391e5c14df276883ad656b
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-04 17:44:21 -0600

    gettingt correct characters with helper function

--- recursion/keypad_combinations.py
+++ recursion/keypad_combinations.py
@@ -20,46 +20,61 @@ def get_characters(num):
 
 
 def keypad(num):
-
-    # TODO: Write your keypad solution here!
-
-    pass
-
-
-def test_keypad(input, expected_output):
-    if sorted(keypad(input)) == expected_output:
-        print("Yay. We got it right.")
+    # base case to break recursion
+    if num == 0:
+        print([""])
     else:
-        print("Oops! That was incorrect.")
+        # find out all the possible strings
+        # that can be made using digits of input num.
+        # Return these strings in a list.
+        # first, get characters
+        characters = list()
+
+        for digit in str(num):
+            d = get_characters(int(digit))
+            characters.append(d)
+
+        print(characters)
+
+
+keypad(0)  # ['']
+keypad(8)  # ["t", "u", "v"]
+keypad(23)  # ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+# def test_keypad(input, expected_output):
+#     if sorted(keypad(input)) == expected_output:
+#         print("Yay. We got it right.")
+#     else:
+#         print("Oops! That was incorrect.")
 
 
 # Base case: list with empty string
-input = 0
-expected_output = [""]
-test_keypad(input, expected_output)
+# input = 0
+# expected_output = [""]
+# test_keypad(input, expected_output)
 
 
-# Example case
-input = 23
-expected_output = sorted(
-    ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
-test_keypad(input, expected_output)
+# # Example case
+# input = 23
+# expected_output = sorted(
+#     ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+# test_keypad(input, expected_output)
 
 
-# Example case
-input = 32
-expected_output = sorted(
-    ["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
-test_keypad(input, expected_output)
+# # Example case
+# input = 32
+# expected_output = sorted(
+#     ["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
+# test_keypad(input, expected_output)
 
 
-# Example case
-input = 8
-expected_output = sorted(["t", "u", "v"])
-test_keypad(input, expected_output)
+# # Example case
+# input = 8
+# expected_output = sorted(["t", "u", "v"])
+# test_keypad(input, expected_output)
 
 
-input = 354
-expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh",
-                          "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
-test_keypad(input, expected_output)
+# input = 354
+# expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh",
+#                           "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
+# test_keypad(input, expected_output)
