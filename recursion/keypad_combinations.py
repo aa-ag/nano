9e04aba06de88e60bd4d891d773ce53a49c14182
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-04 18:13:06 -0600

    passing tests, class code

--- recursion/keypad_combinations.py
+++ recursion/keypad_combinations.py
@@ -19,56 +19,76 @@ def get_characters(num):
         return ""
 
 
+# Recursive Solution
 def keypad(num):
-    # base case to break recursion
-    if num == 0:
+    if num <= 1:
         return [""]
 
+    # If `num` is single digit, get the LIST having one element - the associated string
     elif 1 < num <= 9:
         return list(get_characters(num))
 
+    # Otherwise `num` >= 10. Find the unit's (last) digits of `num`
+    last_digit = num % 10
+
+    '''Step 1'''
+    # Recursive call to the same function with “floor” of the `num//10`
+    small_output = keypad(num//10)               # returns a LIST of strings
+
+    '''Step 2'''
+    # Get the associated string for the `last_digit`
+    keypad_string = get_characters(last_digit)   # returns a string
+
+    '''Permute the characters of result obtained from Step 1 and Step 2'''
+    output = list()
+
+    '''
+    The Idea:
+    Each character of keypad_string must be appended to the 
+    end of each string available in the small_output
+    '''
+    for character in keypad_string:
+        for item in small_output:
+            new_item = item + character
+            output.append(new_item)
+
+    return output                                # returns a LIST of strings
+
+
+def test_keypad(input, expected_output):
+    if sorted(keypad(input)) == expected_output:
+        print("Yay. We got it right.")
     else:
-        pass
-
-
-keypad(0)  # ['']
-keypad(8)  # ["t", "u", "v"]
-keypad(23)  # ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-
-# def test_keypad(input, expected_output):
-#     if sorted(keypad(input)) == expected_output:
-#         print("Yay. We got it right.")
-#     else:
-#         print("Oops! That was incorrect.")
+        print("Oops! That was incorrect.")
 
 
 # Base case: list with empty string
-# input = 0
-# expected_output = [""]
-# test_keypad(input, expected_output)
+input = 0
+expected_output = [""]
+test_keypad(input, expected_output)
 
 
-# # Example case
-# input = 23
-# expected_output = sorted(
-#     ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
-# test_keypad(input, expected_output)
+# Example case
+input = 23
+expected_output = sorted(
+    ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+test_keypad(input, expected_output)
 
 
-# # Example case
-# input = 32
-# expected_output = sorted(
-#     ["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
-# test_keypad(input, expected_output)
+# Example case
+input = 32
+expected_output = sorted(
+    ["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
+test_keypad(input, expected_output)
 
 
-# # Example case
-# input = 8
-# expected_output = sorted(["t", "u", "v"])
-# test_keypad(input, expected_output)
+# Example case
+input = 8
+expected_output = sorted(["t", "u", "v"])
+test_keypad(input, expected_output)
 
 
-# input = 354
-# expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh",
-#                           "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
-# test_keypad(input, expected_output)
+input = 354
+expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh",
+                          "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
+test_keypad(input, expected_output)
