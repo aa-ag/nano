b7e7b7e43006b02822ac0a12cc768a74cd7b5142
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-05 20:02:40 -0600

    print_integers.py

--- recursion/call_stack.py
+++ recursion/call_stack.py
@@ -1,11 +1,18 @@
-def add(num_one, num_two):
-    output = num_one + num_two
-    custom_print(output, num_one, num_two)
-    return output
+# def add(num_one, num_two):
+#     output = num_one + num_two
+#     custom_print(output, num_one, num_two)
+#     return output
 
 
-def custom_print(output, num_one, num_two):
-    print("The sum of {} and {} is: {}".format(num_one, num_two, output))
+# def custom_print(output, num_one, num_two):
+#     print("The sum of {} and {} is: {}".format(num_one, num_two, output))
 
 
-result = add(5, 7)
+# result = add(5, 7)
+
+def print_integers(n):
+    for i in range(n, 0, -1):
+        print(i)
+
+
+print_integers(5)
