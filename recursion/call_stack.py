bbc357c9d8e5a9d912b88d2257282ac97a0222c7
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-05 19:23:50 -0600

    add()

--- recursion/call_stack.py
+++ recursion/call_stack.py
@@ -1,7 +1,11 @@
 def add(num_one, num_two):
     output = num_one + num_two
+    custom_print(output, num_one, num_two)
     return output
 
 
+def custom_print(output, num_one, num_two):
+    print("The sum of {} and {} is: {}".format(num_one, num_two, output))
+
+
 result = add(5, 7)
-print(result)
