8a1ebdc734b8a6d583172b4b7ec6dc8bdbe12e35
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-05 21:35:58 -0600

    class code

--- recursion/return_codes.py
+++ recursion/return_codes.py
@@ -1,5 +1,38 @@
+def get_alphabet(number):
+    return chr(number + 96)
+
+
 def all_codes(number):
-    pass
+    if number == 0:
+        return [""]
+
+    # calculation for two right-most digits e.g. if number = 1123, this calculation is meant for 23
+    remainder = number % 100
+    output_100 = list()
+    if remainder <= 26 and number > 9:
+
+        # get all codes for the remaining number
+        output_100 = all_codes(number // 100)
+        alphabet = get_alphabet(remainder)
+
+        for index, element in enumerate(output_100):
+            output_100[index] = element + alphabet
+
+    # calculation for right-most digit e.g. if number = 1123, this calculation is meant for 3
+    remainder = number % 10
+
+    # get all codes for the remaining number
+    output_10 = all_codes(number // 10)
+    alphabet = get_alphabet(remainder)
+
+    for index, element in enumerate(output_10):
+        output_10[index] = element + alphabet
+
+    output = list()
+    output.extend(output_100)
+    output.extend(output_10)
+
+    return output
 
 
 def test_function(test_case):
