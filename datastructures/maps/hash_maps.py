651270be53acc51d87fa0e54f1924fbc7fa18e22
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-08 19:58:46 -0600

    hashmap as a class

--- datastructures/maps/hash_maps.py
+++ datastructures/maps/hash_maps.py
@@ -1,10 +1,45 @@
-def hash_function(string):
-    hash_code = 0
-    for character in string:
-        hash_code += ord(character)
-    return hash_code
+class HashMap:
+
+    def __init__(self, initial_size=10):
+        self.bucket_array = [None for _ in range(initial_size)]
+        self.p = 37                  # a prime numbers
+        self.num_entries = 0
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
+
+    # Returns the bucket_index
+    def get_bucket_index(self, key):
+        # The returned hash code will be the bucket_index
+        return self.get_hash_code(key)
+
+    # Returns the hash code
+
+    def get_hash_code(self, key):
+        key = str(key)
+
+        # represents (self.p^0) which is 1
+        current_coefficient = 1
+
+        hash_code = 0
+
+        for character in key:
+            hash_code += ord(character) * current_coefficient
+            current_coefficient *= self.p
+
+        return hash_code  # The generated hash code will be the bucket_index
 
 
-hash_code_1 = hash_function("abcd")
-print(hash_code_1)
-# 394
+hash_map = HashMap()
+
+bucket_index = hash_map.get_bucket_index("abcd")
+print(bucket_index)
+
+
+hash_map = HashMap()
+
+bucket_index = hash_map.get_bucket_index("bcda")
+print(bucket_index)
