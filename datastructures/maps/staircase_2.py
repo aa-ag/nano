0dd561300b432132cc6e3baf0d408b6644759101
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-09 22:33:29 -0600

    staircase_2.py faster implementation

--- datastructures/maps/staircase_2.py
+++ datastructures/maps/staircase_2.py
@@ -1,12 +1,35 @@
 def staircase(n):
-    if n <= 0:
-        return 1
-    elif n == 1 or n == 2:
-        return n
-    elif n == 3:
-        return 4
+    num_dict = dict({})
+    return staircase_faster(n, num_dict)
 
-    return staircase(n - 1) + staircase(n - 2) + staircase(n - 3)
+
+def staircase_faster(n, num_dict):
+    if n == 1:
+        output = 1
+    elif n == 2:
+        output = 2
+    elif n == 3:
+        output = 4
+    else:
+        if (n - 1) in num_dict:
+            first_output = num_dict[n - 1]
+        else:
+            first_output = staircase_faster(n - 1, num_dict)
+
+        if (n - 2) in num_dict:
+            second_output = num_dict[n - 2]
+        else:
+            second_output = staircase_faster(n - 2, num_dict)
+
+        if (n - 3) in num_dict:
+            third_output = num_dict[n - 3]
+        else:
+            third_output = staircase_faster(n - 3, num_dict)
+
+        output = first_output + second_output + third_output
+
+    num_dict[n] = output
+    return output
 
 
 # Tests
