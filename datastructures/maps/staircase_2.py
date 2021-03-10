b2a48d3cb53d2801019a48d28cd635b5a8e3b4e5
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-09 22:29:50 -0600

    staircase_2.py solution

--- datastructures/maps/staircase_2.py
+++ datastructures/maps/staircase_2.py
@@ -1,15 +1,12 @@
 def staircase(n):
-    # Base Case - What holds true for minimum steps possible
-    # i.e., n == 1? Return the number of ways the child can
-    # climb one step.
+    if n <= 0:
+        return 1
+    elif n == 1 or n == 2:
+        return n
+    elif n == 3:
+        return 4
 
-    # Inductive Hypothesis - What holds true for n == 2 or n == 3?
-    # Return the number of ways to climb them.
-
-    # Inductive Step (n > 3) - use Inductive Hypothesis
-    # to formulate a solution
-
-    pass
+    return staircase(n - 1) + staircase(n - 2) + staircase(n - 3)
 
 
 # Tests
