0d7e71510cb76ddcf7114470e51ae11b20770259
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-11 21:11:16 -0600

    setting left & right children

--- datastructures/trees/trees_intro.py
+++ datastructures/trees/trees_intro.py
@@ -7,13 +7,22 @@ class Node:
     def get_value(self):
         print(self.value)
 
+    def set_right_child(self, new_value):
+        self.right = new_value
+
+    def set_left_child(self, new_value):
+        self.left = new_value
+
 
 # Check
-
 node0 = Node("apple")
 node1 = Node("banana")
 node2 = Node("orange")
+node0.set_left_child(node1)
+node0.set_right_child(node2)
 
-node0.get_value()
-node1.get_value()
-node2.get_value()
+print(f"""
+node 0: {node0.value}
+node 0 left child: {node0.left.value}
+node 0 right child: {node0.right.value}
+""")
