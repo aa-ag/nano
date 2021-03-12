b4150059e86bfdfd915bc33f7716c1c04dd34e32
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-11 20:27:11 -0600

    testing constructor for trees_intro.py's Node class

--- datastructures/trees/trees_intro.py
+++ datastructures/trees/trees_intro.py
@@ -1,6 +1,22 @@
-# Define a node
+# Task 02: add a constructor that takes the value as a parameter
 class Node:
-    def __init__(self):
-        self.value = None
+    def __init__(self, value=None):
+        self.value = value
         self.left = None
         self.right = None
+
+
+# tests
+node0 = Node()
+print(f"""
+value: {node0.value}
+left: {node0.left}
+right: {node0.right}
+""")
+
+node0 = Node("apple")
+print(f"""
+value: {node0.value}
+left: {node0.left}
+right: {node0.right}
+""")
