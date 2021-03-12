655b258ca4ee9e957dfc20f3cca0548b3e9d39ee
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-11 21:13:34 -0600

    cadded has_left_child() & has_right_child()

--- datastructures/trees/trees_intro.py
+++ datastructures/trees/trees_intro.py
@@ -13,13 +13,27 @@ class Node:
     def set_left_child(self, new_value):
         self.left = new_value
 
+    def has_left_child(self):
+        return self.left != None
+
+    def has_right_child(self):
+        return self.right != None
+
 
 # Check
 node0 = Node("apple")
 node1 = Node("banana")
 node2 = Node("orange")
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
+
+print("adding left and right children")
 node0.set_left_child(node1)
 node0.set_right_child(node2)
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
 
 print(f"""
 node 0: {node0.value}
