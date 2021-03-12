cc3b6117b104d7854056aefb03d88900ad7017f7
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-11 21:24:20 -0600

    BinaryTree class

--- datastructures/trees/trees_intro.py
+++ datastructures/trees/trees_intro.py
@@ -20,23 +20,14 @@ class Node:
         return self.right != None
 
 
-# Check
-node0 = Node("apple")
-node1 = Node("banana")
-node2 = Node("orange")
+# Create Binary Tree
+class BinaryTree():
+    def __init__(self):
+        '''
+         Class names should normally use the CapWords convention.
+         https://www.python.org/dev/peps/pep-0008/#id41
+        '''
+        self.root = None
 
-print(f"has left child? {node0.has_left_child()}")
-print(f"has right child? {node0.has_right_child()}")
-
-print("adding left and right children")
-node0.set_left_child(node1)
-node0.set_right_child(node2)
-
-print(f"has left child? {node0.has_left_child()}")
-print(f"has right child? {node0.has_right_child()}")
-
-print(f"""
-node 0: {node0.value}
-node 0 left child: {node0.left.value}
-node 0 right child: {node0.right.value}
-""")
+    def get_root(self):
+        return self.root
