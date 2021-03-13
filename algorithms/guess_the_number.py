64ea683f7898ffdc7406187f80f71d146795577d
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-13 12:41:05 -0600

    guess_the_number() within new algorithms section

---
+++ algorithms/guess_the_number.py
@@ -0,0 +1,16 @@
+###--- IMPORTS ---###
+import random
+
+
+###--- CODE ---###
+def guess_the_number():
+    '''
+     guess a random number between 1 and 100, 
+     you will have 7 tries
+    '''
+    pass
+
+
+###--- DRIVER CODE ---###
+if __name__ == '__main__':
+    guess_the_number()
