689fd018706c46570635b57d688130ba73023399
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-13 12:50:44 -0600

    added try-except to handle inputs that aren't numbers

--- algorithms/guess_the_number.py
+++ algorithms/guess_the_number.py
@@ -19,24 +19,32 @@ def guess_the_number(total_tries, start_range, end_range):
         start_range, end_range = end_range, start_range
 
     random_number = random.randint(start_range, end_range)
+
     try_count = 0
+
     success_message = "Awesome! You guessed correctly"
     failure_message = "Sorry! No more retries left"
     miss_message = "Oops! That's incorrect"
 
     num_tries = 0
-    while num_tries < total_tries:
-        attempt = int(input("Guess the number: "))
 
-        if attempt == random_number:
-            print(success_message)
-            return
-        print(miss_message)
-        if attempt < random_number:
-            print("Go higher!")
-        else:
-            print("Go lower!")
-        num_tries += 1
+    while num_tries < total_tries:
+        try:
+            attempt = int(input("Guess the number: "))
+
+            if attempt == random_number:
+                print(success_message)
+                return
+            print(miss_message)
+
+            if attempt < random_number:
+                print("Go higher!")
+            else:
+                print("Go lower!")
+            num_tries += 1
+        except:
+            print("Don't know how to handle that, pls enter a number")
+
     print(failure_message)
 
 
