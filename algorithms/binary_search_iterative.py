dc6df2dcae996d843292a0072495a430268d6ad7
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-14 22:45:35 -0500

    added binary_search_recursion.py's base code

--- algorithms/binary_search_iterative.py
+++ algorithms/binary_search_iterative.py
@@ -50,3 +50,10 @@ target = 6
 index = 6
 test_case = [array, target, index]
 test_function(test_case)
+
+
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 11
+index = 11
+test_case = [array, target, index]
+test_function(test_case)
