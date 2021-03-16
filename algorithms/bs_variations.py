18f3ddb0609073f3ecb4f4bd0dc24865637e8034
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-15 19:33:15 -0500

    solution to find_first without docstring

--- algorithms/bs_variations.py
+++ algorithms/bs_variations.py
@@ -10,5 +10,33 @@ def recursive_binary_search(target, source, left=0):
         return recursive_binary_search(target, source[:center], left)
 
 
-multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12]
-recursive_binary_search(7, multiple)
+def find_first(target, source):
+    i = recursive_binary_search(target, source)
+
+    if not i:
+        return None
+    while source[i] == target:
+        if i == 0:
+            return 0
+        if source[i - 1] == target:
+            i -= 1
+        else:
+            return i
+
+
+def find_first(target, source):
+    index = recursive_binary_search(target, source)
+    if not index:
+        return None
+    while source[index] == target:
+        if index == 0:
+            return 0
+        if source[index-1] == target:
+            index -= 1
+        else:
+            return index
+
+
+multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+print(find_first(7, multiple))  # Should return 3
+print(find_first(9, multiple))  # Should return None
