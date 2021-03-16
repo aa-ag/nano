cb3a41eb0a4c69f198f11f430144fd6d81c8977e
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-16 05:45:23 -0500

    added pre_order_traversal_recursive.py -- traversing tree using DFS + recursion

---
+++ datastructures/trees/pre_order_traversal_recursive.py
@@ -0,0 +1,107 @@
+class Node():
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left != None
+
+    def has_right_child(self):
+        return self.right != None
+
+    # define __repr_ to decide what a print statement displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+class Stack():
+    def __init__(self):
+        self.list = list()
+
+    def push(self, value):
+        self.list.append(value)
+
+    def pop(self):
+        return self.list.pop()
+
+    def top(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.list) == 0
+
+    def __repr__(self):
+        if len(self.list) > 0:
+            s = "<top of stack>\n_________________\n"
+            s += "\n_________________\n".join([str(item)
+                                               for item in self.list[::-1]])
+            s += "\n_________________\n<bottom of stack>"
+            return s
+
+        else:
+            return "<stack is empty>"
+
+
+def pre_order(tree):
+    # first, set a list to track
+    # visit order
+    visit_order = list()
+
+    # then have a nested function
+    # traverse()
+    def traverse(node):
+        if node:
+            # visit the node
+            visit_order.append(node.get_value())
+
+            # traverse left subtree
+            traverse(node.get_left_child())
+
+            # traverse right subtree
+            traverse(node.get_right_child())
+
+    traverse(tree.get_root())
+
+    return visit_order
+
+
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+pre_order(tree)
