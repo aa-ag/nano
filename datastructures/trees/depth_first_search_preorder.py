682c89264df64d6cbc745e3071b889cff408847c
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-16 04:54:13 -0500

    added code Stack() class, necesary for DFS Binary Search

--- datastructures/trees/depth_first_search_preorder.py
+++ datastructures/trees/depth_first_search_preorder.py
@@ -1,3 +1,35 @@
+# Let's define a stack to help keep track of the tree nodes
+class Stack():
+    def __init__(self):
+        self.list = list()
+
+    def push(self, value):
+        self.list.append(value)
+
+    def pop(self):
+        return self.list.pop()
+
+    def top(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.list) == 0
+
+    def __repr__(self):
+        if len(self.list) > 0:
+            s = "<top of stack>\n_________________\n"
+            s += "\n_________________\n".join([str(item)
+                                               for item in self.list[::-1]])
+            s += "\n_________________\n<bottom of stack>"
+            return s
+
+        else:
+            return "<stack is empty>"
+
+
 def pre_order_with_stack_buggy(tree):
     visit_order = list()
     stack = Stack()
@@ -9,16 +41,17 @@ def pre_order_with_stack_buggy(tree):
     loop_limit = 7
     while(node and count < loop_limit):
         print(f"""
-loop count: {count}
-current node: {node}
-stack:
-{stack}
-        """)
+            loop count: {count}
+            current node: {node}
+            stack:
+            {stack}
+            """)
         count += 1
         if node.has_left_child():
             node = node.get_left_child()
             stack.push(node)
-            # using top() is redundant, but added for clarity
+            # using top() is redundant,
+            # but added for clarity
             node = stack.top()
             visit_order.append(node.get_value())
 
