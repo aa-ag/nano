d83cc15af2dfc56eb14391929100c06a4f107f4f
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-15 19:42:19 -0500

    recursive contains using binary search

--- algorithms/contains.py
+++ algorithms/contains.py
@@ -1,5 +1,17 @@
+def recursive_binary_search(target, source, left=0):
+    if len(source) == 0:
+        return None
+    center = (len(source)-1) // 2
+    if source[center] == target:
+        return center + left
+    elif source[center] < target:
+        return recursive_binary_search(target, source[center+1:], left+center+1)
+    else:
+        return recursive_binary_search(target, source[:center], left)
+
+
 def contains(target, source):
-    pass
+    return recursive_binary_search(target, source) != None
 
 
 letters = ['a', 'c', 'd', 'f', 'g']
