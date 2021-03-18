a2a4ac7ac3cff5b8f73ee6859352f77ad7056673
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-17 20:33:49 -0500

    conducting bfs using while loop

--- datastructures/trees/breath_first_search.py
+++ datastructures/trees/breath_first_search.py
@@ -82,66 +82,27 @@ class Queue():
             return "<queue is empty>"
 
 
-visit_order = list()
-q = Queue()
+# BFS algorithm
+def bfs(tree):
+    visit_order = list()
+    q = Queue()
 
-# start at the root node and add it to the queue
-node = tree.get_root()
-q.enq(node)
-print(q)
+    node = tree.get_root()
+    q.enq(node)
 
-# dequeue the next node in the queue.
-# "visit" that node
-# also add its children to the queue
+    while len(q) > 0:
 
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
+        node = q.deq()
+        visit_order.append(node)
 
-print(f"visit order: {visit_order}")
-print(q)
+        if node.has_left_child():
+            q.enq(node.get_left_child())
+        if node.has_right_child():
+            q.enq(node.get_right_child())
 
-
-# dequeue the next node (banana)
-# visit it, and add its children (dates) to the queue
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-
-print(f"visit order: {visit_order}")
-print(q)
+    print(f"visit order: {visit_order}")
+    print(q)
 
 
-# dequeue the next node (cherry)
-# visit it, and add its children (there are None) to the queue
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-
-print(f"visit order: {visit_order}")
-print(q)
-
-
-# dequeue the next node (dates)
-# visit it, and add its children (there are None) to the queue
-
-node = q.deq()
-visit_order.append(node)
-if node.has_left_child():
-    q.enq(node.get_left_child())
-if node.has_right_child():
-    q.enq(node.get_right_child())
-
-print(f"visit order: {visit_order}")
-print(q)
+# check solution: should be: apple, banana, cherry, dates
+bfs(tree)
