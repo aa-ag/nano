b0400fb474e15d8dd940bc19af0f4665a3bdf463
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-18 20:48:11 -0500

    implementing trie as class, base code from class

---
+++ algorithms/trie_class.py
@@ -0,0 +1,38 @@
+class TrieNode(object):
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        """
+        Add `word` to trie
+        """
+        pass
+
+    def exists(self, word):
+        """
+        Check if word exists in trie
+        """
+        pass
+
+
+word_list = ['apple', 'bear', 'goo', 'good',
+             'goodbye', 'goods', 'goodwill', 'gooses', 'zebra']
+word_trie = Trie()
+
+# Add words
+for word in word_list:
+    word_trie.add(word)
+
+# Test words
+test_words = ['bear', 'goo', 'good', 'goos']
+for word in test_words:
+    if word_trie.exists(word):
+        print('"{}" is a word.'.format(word))
+    else:
+        print('"{}" is not a word.'.format(word))
