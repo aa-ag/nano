ae68ec6ebac882feaffd8480f14f4a88e8725b61
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-18 20:53:04 -0500

    another option to implement tries, besides manually, and using a class, is using Defaultdict from collections module

---
+++ algorithms/trie_defaultdict.py
@@ -0,0 +1,38 @@
+import collections
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.is_word = False
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        """
+        Add `word` to trie
+        """
+        pass
+
+    def exists(self, word):
+        """
+        Check if word exists in trie
+        """
+        pass
+
+
+# Add words
+valid_words = ['the', 'a', 'there', 'answer', 'any', 'by', 'bye', 'their']
+word_trie = Trie()
+for valid_word in valid_words:
+    word_trie.add(valid_word)
+
+# Tests
+assert word_trie.exists('the')
+assert word_trie.exists('any')
+assert not word_trie.exists('these')
+assert not word_trie.exists('zzz')
+print('All tests passed!')
