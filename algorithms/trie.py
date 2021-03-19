1a138e7b5e2bae857306d47e016ee28ce4558ddd
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-18 20:46:37 -0500

    testing basic Trie: seeing how it saves memory by overlapping elements (in this case words) which makes them work as nodes

--- algorithms/trie.py
+++ algorithms/trie.py
@@ -11,6 +11,30 @@ basic_trie = {
         'word_end': False}}
 
 
-print('Is "a"   a word: {}'.format(basic_trie['a']['word_end']))
-print('Is "ad"  a word: {}'.format(basic_trie['a']['d']['word_end']))
-print('Is "add" a word: {}'.format(basic_trie['a']['d']['d']['word_end']))
+def is_word(word):
+    """
+    Look for the word in `basic_trie`
+    """
+    current_node = basic_trie
+
+    for char in word:
+        if char not in current_node:
+            return False
+
+        current_node = current_node[char]
+
+    return current_node['word_end']
+
+
+# Test words
+
+def test(test_words):
+    for word in test_words:
+        if is_word(word):
+            print(f'"{word}" is a word.')
+        else:
+            print(f'"{word}" is not a word.')
+
+
+if __name__ == '__main__':
+    test(['ap', 'add'])
