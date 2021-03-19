aa7311915bf872f1b45833a9d7baa369cf9559e5
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-18 20:55:47 -0500

    implemented Trie using collections' defaultdict -- class code

--- algorithms/trie_defaultdict.py
+++ algorithms/trie_defaultdict.py
@@ -15,13 +15,26 @@ class Trie(object):
         """
         Add `word` to trie
         """
-        pass
+        current_node = self.root
+
+        for char in word:
+            current_node = current_node.children[char]
+
+            current_node.is_word = True
 
     def exists(self, word):
         """
         Check if word exists in trie
         """
-        pass
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                return False
+
+            current_node = current_node.children[char]
+
+        return current_node.is_word
 
 
 # Add words
