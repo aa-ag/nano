a07211d026b58938e725db1e862415d0d6f4c026
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-19 19:18:31 -0500

    added diameter_of_binary_tree with base code from class; trygin to find max distance between any two nodes given a root

---
+++ datastructures/trees/diameter_of_binary_tree.py
@@ -0,0 +1,14 @@
+# Note: Diameter of a Binary Tree
+# is the maximum distance between
+# any two nodes
+
+class BinaryTreeNode:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+def diameter_of_binary_tree(root):
+    pass
