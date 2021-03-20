44310a5ee3b7ca2e469a2dbfb92d1dee55f37bbe
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-20 14:10:53 -0500

    added Heap class

---
+++ algorithms/heaps.py
@@ -0,0 +1,12 @@
+class Heap:
+    def __init__(self, initial_size):
+        # initialize arrays
+        self.cbt = [None for _ in range(initial_size)]
+        # denotes next index where new element should go
+        self.next_index = 0
+
+    def insert(self, data):
+        pass
+
+    def remove(self):
+        pass
