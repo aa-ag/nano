ca8a6d4504f27c6ef74291a6306a458ebef58d85
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-20 21:58:01 -0500

    added test from class code: heap_size of 5

--- algorithms/heaps.py
+++ algorithms/heaps.py
@@ -108,3 +108,28 @@ class Heap:
         if self.size() == 0:
             return None
         return self.cbt[0]
+
+
+# test
+
+heap_size = 5
+heap = Heap(heap_size)
+
+elements = [1, 2, 3, 4, 1, 2]
+for element in elements:
+    heap.insert(element)
+print('Inserted elements: {}'.format(elements))
+
+print('size of heap: {}'.format(heap.size()))
+
+for _ in range(4):
+    print('Call remove: {}'.format(heap.remove()))
+
+print('Call get_minimum: {}'.format(heap.get_minimum()))
+
+for _ in range(2):
+    print('Call remove: {}'.format(heap.remove()))
+
+print('size of heap: {}'.format(heap.size()))
+print('Call remove: {}'.format(heap.remove()))
+print('Call is_empty: {}'.format(heap.is_empty()))
