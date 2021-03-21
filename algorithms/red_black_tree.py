00af12218f931ac9b0c3fb7da36d8d73160e4b3e
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-21 14:52:59 -0500

    added insert() and insert helpert functions

--- algorithms/red_black_tree.py
+++ algorithms/red_black_tree.py
@@ -12,7 +12,19 @@ class RedBlackTree(object):
         self.root = Node(root, None, 'red')
 
     def insert(self, new_val):
-        pass
+        self.insert_helper(self.root, new_val)
+
+    def insert_helper(self, current, new_val):
+        if current.value < new_val:
+            if current.right:
+                self.insert_helper(current.right, current, new_val)
+            else:
+                current.right = Node(new_val, current, 'red')
+        else:
+            if current.left:
+                self.insert_helper(current.left, current, new_val)
+            else:
+                current.left = Node(new_val, current, 'red')
 
     def search(self, find_val):
         return False
