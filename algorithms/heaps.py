d2d73a06d250e6865c57066dff1c733f520ea81c
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-20 21:50:57 -0500

    added insert() method in Heap class

--- algorithms/heaps.py
+++ algorithms/heaps.py
@@ -1,12 +1,22 @@
 class Heap:
-    def __init__(self, initial_size):
-        # initialize arrays
-        self.cbt = [None for _ in range(initial_size)]
-        # denotes next index where new element should go
-        self.next_index = 0
+    def __init__(self, initial_size=10):
+        self.cbt = [None for _ in range(initial_size)]  # initialize arrays
+        self.next_index = 0  # denotes next index where new element should go
 
     def insert(self, data):
-        pass
+        # insert element at the next index
+        self.cbt[self.next_index] = data
 
-    def remove(self):
-        pass
+        # heapify
+        self._up_heapify()
+
+        # increase index by 1
+        self.next_index += 1
+
+        # double the array and copy elements if next_index goes out of array bounds
+        if self.next_index >= len(self.cbt):
+            temp = self.cbt
+            self.cbt = [None for _ in range(2 * len(self.cbt))]
+
+            for index in range(self.next_index):
+                self.cbt[index] = temp[index]
