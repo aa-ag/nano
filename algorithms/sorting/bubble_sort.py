a316006fd2540e31d115ffe88ae5110aa64d474a
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-23 20:12:17 -0500

    added bubble_sort.py: implementation of bubble sort, exercise 1, boilerplate

---
+++ algorithms/sorting/bubble_sort.py
@@ -0,0 +1,18 @@
+# Exercise 1
+'''
+ Sam records when they wake up every morning. 
+ Assuming Sam always wakes up in the same hour, 
+ use bubble sort to sort by earliest to latest.
+
+'''
+
+
+def bubble_sort_1(l):
+    pass
+
+
+wakeup_times = [16, 49, 3, 12, 56, 49, 55, 22,
+                13, 46, 19, 55, 46, 13, 25, 56, 9, 48, 45]
+
+bubble_sort_1(wakeup_times)
+print("Pass" if (wakeup_times[0] == 3) else "Fail")
