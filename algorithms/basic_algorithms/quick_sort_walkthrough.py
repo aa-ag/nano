091d9a2661bee04221f1495bcc325cff183d3fe8
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-28 17:41:34 -0500

    added 2 additional tests to quick_sort_walkthrough.py

--- algorithms/basic_algorithms/quick_sort_walkthrough.py
+++ algorithms/basic_algorithms/quick_sort_walkthrough.py
@@ -1,6 +1,6 @@
-def sort_a_little_bit(items):
-    left_index = 0
-    pivot_index = len(items) - 1
+def sort_a_little_bit(items, begin_index, end_index):
+    left_index = begin_index
+    pivot_index = end_index
     pivot_value = items[pivot_index]
 
     while (pivot_index != left_index):
@@ -16,7 +16,34 @@ def sort_a_little_bit(items):
         items[pivot_index] = item
         pivot_index -= 1
 
+    return pivot_index
 
+
+def sort_all(items, begin_index, end_index):
+    if end_index <= begin_index:
+        return
+
+    pivot_index = sort_a_little_bit(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index - 1)
+    sort_all(items, pivot_index + 1, end_index)
+
+
+def quicksort(items):
+    sort_all(items, 0, len(items) - 1)
+
+
+# testing
+# test 1
 items = [8, 3, 1, 7, 0, 10, 2]
-sort_a_little_bit(items)
+quicksort(items)
+print(items)
+
+# test 2
+items = [1, 0]
+quicksort(items)
+print(items)
+
+# test 3
+items = [96, 97, 98]
+quicksort(items)
 print(items)
