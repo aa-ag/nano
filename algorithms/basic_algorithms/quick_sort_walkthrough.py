e207878ab4e3df181b48ba6a752875af0dd89c94
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-28 17:37:39 -0500

    refactored quicksort into function

--- algorithms/basic_algorithms/quick_sort_walkthrough.py
+++ algorithms/basic_algorithms/quick_sort_walkthrough.py
@@ -1,26 +1,22 @@
+def sort_a_little_bit(items):
+    left_index = 0
+    pivot_index = len(items) - 1
+    pivot_value = items[pivot_index]
+
+    while (pivot_index != left_index):
+
+        item = items[left_index]
+
+        if item <= pivot_value:
+            left_index += 1
+            continue
+
+        items[left_index] = items[pivot_index - 1]
+        items[pivot_index - 1] = pivot_value
+        items[pivot_index] = item
+        pivot_index -= 1
+
+
 items = [8, 3, 1, 7, 0, 10, 2]
-
-pivot_index = len(items) - 1
-pivot_value = items[pivot_index]
-
-left_index = 0
-
-while (pivot_index != left_index):
-
-    item = items[left_index]
-
-    if item <= pivot_value:
-        left_index += 1
-        continue
-
-    # Place the item before the pivot at left_index
-    items[left_index] = items[pivot_index - 1]
-    # Shift pivot one to the left
-    items[pivot_index - 1] = pivot_value
-    # Place item at pivot's previous location
-    items[pivot_index] = item
-    # Update pivot_index
-    pivot_index -= 1
-
+sort_a_little_bit(items)
 print(items)
-# [0, 1, 2, 7, 3, 10, 8]
