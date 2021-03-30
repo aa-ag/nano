0b7c8809156b1da9cbf2d7d77ed1198cbd1b9d35
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-30 17:41:33 -0500

    added sort012.py and it's boilerplate code from class

---
+++ algorithms/basic_algorithms/sort012.py
@@ -0,0 +1,40 @@
+'''
+### Problem Statement
+
+Write a function that takes an input array
+(or Python list) consisting of only `0`s, `1`s, and `2`s, 
+and sorts that array in a single traversal.
+
+Note that if you can get the function 
+to put the `0`s and `2`s in the correct positions, 
+this will aotumatically cause the `1`s 
+to be in the correct positions as well.
+'''
+
+
+def sort_012(input_list):
+    pass
+
+
+# tests
+def test_function(test_case):
+    sort_012(test_case)
+    print(test_case)
+    if test_case == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+# test 1
+test_case = [0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]
+test_function(test_case)
+
+# test 2
+test_case = [2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0,
+             2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]
+test_function(test_case)
+
+# test 3
+test_case = [2, 2, 0, 0, 2, 1, 0, 2, 2, 1, 1, 1, 0, 1, 2, 0, 2, 0, 1]
+test_function(test_case)
