06ed3b80294441bbd258ca960ee35fbe1650f824
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-31 03:07:21 -0500

    added kth_largest.py to basic algorithms section

---
+++ algorithms/basic_algorithms/kth_largest.py
@@ -0,0 +1,19 @@
+def fast_select(arr, k):
+    '''TO DO'''
+    # Implement the algorithm explained above to find the k^th lasrgest element in the given array
+    pass
+
+
+arr = [6, 80, 36, 8, 23, 7, 10, 12, 42]
+k = 5
+print(fast_select(arr, k))  # Outputs 12
+
+
+arr = [5, 2, 20, 17, 11, 13, 8, 9, 11]
+k = 5
+print(fast_select(arr, k))  # Outputs 11
+
+
+arr = [6, 80, 36, 8, 23, 7, 10, 12, 42, 99]
+k = 10
+print(fast_select(arr, k))  # Outputs 99
--- algorithms/basic_algorithms/sort012.py
+++ algorithms/basic_algorithms/sort012.py
@@ -1,32 +1,26 @@
-'''
-### Problem Statement
-
-Write a function that takes an input array
-(or Python list) consisting of only `0`s, `1`s, and `2`s, 
-and sorts that array in a single traversal.
-
-Note that if you can get the function 
-to put the `0`s and `2`s in the correct positions, 
-this will aotumatically cause the `1`s 
-to be in the correct positions as well.
-'''
-
-
 def sort_012(input_list):
-    zeros = list()
-    ones = list()
-    twos = list()
+    """
+    The idea is to put 0 and 2 in their correct positions, which will make sure
+    all the 1s are automatically placed in their right positions
+    """
+    # initialize pointers for next positions of 0 and 2
+    next_pos_0 = 0
+    next_pos_2 = len(input_list) - 1
 
-    for i in input_list:
-        if i == 0:
-            zeros.append(i)
-        elif i == 1:
-            ones.append(i)
-        elif i == 2:
-            twos.append(i)
+    front_index = 0
 
-    answer = zeros + ones + twos
-    return answer
+    while front_index <= next_pos_2:
+        if input_list[front_index] == 0:
+            input_list[front_index] = input_list[next_pos_0]
+            input_list[next_pos_0] = 0
+            next_pos_0 += 1
+            front_index += 1
+        elif input_list[front_index] == 2:
+            input_list[front_index] = input_list[next_pos_2]
+            input_list[next_pos_2] = 2
+            next_pos_2 -= 1
+        else:
+            front_index += 1
 
 
 # tests
