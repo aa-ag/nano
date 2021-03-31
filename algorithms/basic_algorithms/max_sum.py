d695768ff370389a856f4fc1e06160e67d3d641b
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-31 03:25:41 -0500

    added max_sum.py and its boilerplate code from class

---
+++ algorithms/basic_algorithms/max_sum.py
@@ -0,0 +1,32 @@
+def maxSubArray(arr):
+    '''
+    param: An array `arr`
+    return: The maximum sum of the contiguous subarray. 
+    No need to return the subarray itself.
+    '''
+    pass
+
+
+# Test your code
+arr = [-2, 7, -6, 3, 1, -4, 5, 7]
+print("Maximum Sum = ", maxSubArray(arr))     # Outputs 13
+
+
+# Test your code
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print("Maximum Sum = ", maxSubArray(arr))     # Outputs 6
+
+
+# Test your code
+arr = [-4, 14, -6, 7]
+print("Maximum Sum = ", maxSubArray(arr))     # Outputs 15
+
+
+# Test your code
+arr = [-2, 1, -3, 5, 0, 3, 2, -5, 4]
+print("Maximum Sum = ", maxSubArray(arr))     # Outputs 10
+
+
+# Test your code
+arr = [-2, -5, 6, -2, -3, 1, 5, -6]
+print("Maximum Sum = ", maxSubArray(arr))     # Outputs 7
