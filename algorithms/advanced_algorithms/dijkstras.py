89cf2c31073e54711689bd15aad7f1bd4f670a6b
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-04 18:01:14 -0500

    added dijkstras.py with Graph class boilerplate from class

---
+++ algorithms/advanced_algorithms/dijkstras.py
@@ -0,0 +1,26 @@
+# Helper Code
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()                   # A set cannot contain duplicate nodes
+        # Defaultdict is a child class of Dictionary that provides a default value for a key that does not exists.
+        self.neighbours = defaultdict(list)
+        # Dictionary. An example record as ('A', 'B'): 6 shows the distance between 'A' to 'B' is 6 units
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.neighbours[from_node].append(to_node)
+        self.neighbours[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+        # lets make the graph undirected / bidirectional
+        self.distances[(to_node, from_node)] = distance
+
+    def print_graph(self):
+        print("Set of Nodes are: ", self.nodes)
+        print("Neighbours are: ", self.neighbours)
+        print("Distances are: ", self.distances)
