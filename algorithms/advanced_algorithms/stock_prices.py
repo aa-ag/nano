35f4c6830311a1a1a9fd148cdc8c64dc9b39aa6c
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-11 21:09:30 -0500

    added stock_prices.py and its boilerplate code

---
+++ algorithms/advanced_algorithms/stock_prices.py
@@ -0,0 +1,38 @@
+def max_returns(prices):
+    """
+    Calculate maxiumum possible return
+
+    Args:
+       prices(array): array of prices
+    Returns:
+       int: The maximum profit possible
+    """
+
+    return prices
+
+
+# Test Cases
+def test_function(test_case):
+    prices = test_case[0]
+    solution = test_case[1]
+    output = max_returns(prices)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+prices = [2, 2, 7, 9, 9, 12, 18, 23, 34, 37, 45, 54, 78]
+solution = 76
+test_case = [prices, solution]
+test_function(test_case)
+
+prices = [54, 18, 37, 9, 11, 48, 23, 1, 7, 34, 2, 45, 67]
+solution = 66
+test_case = [prices, solution]
+test_function(test_case)
+
+prices = [78, 54, 45, 37, 34, 23, 18, 12, 9, 9, 7, 2, 2]
+solution = 0
+test_case = [prices, solution]
+test_function(test_case)
