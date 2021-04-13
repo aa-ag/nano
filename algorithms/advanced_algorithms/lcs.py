14f91e6de422ce164ef45e806bd9487dc0050a4e
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-12 20:21:08 -0500

    added docstring with time complexity of lcs()

--- algorithms/advanced_algorithms/lcs.py
+++ algorithms/advanced_algorithms/lcs.py
@@ -1,4 +1,10 @@
 def lcs(string_a, string_b):
+    '''
+    The time complexity of the above implementation is dominated by the two nested loops, 
+    Let the length of string_a and string_b is `n` and `m` respectively. 
+    This would lead to a time complexity of O(n*m). 
+    But, in general, we can consider it as O(n*n) instead of O(n*m).
+    '''
 
     # initialize the matrix
     lookup_table = [[0 for x in range(len(string_b) + 1)]
