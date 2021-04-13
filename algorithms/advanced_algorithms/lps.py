9d197b6508150baacc8657bafde31fb12cf2ca67
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-12 20:25:21 -0500

    added lps' class code within lps.py

--- algorithms/advanced_algorithms/lps.py
+++ algorithms/advanced_algorithms/lps.py
@@ -1,13 +1,45 @@
+import pprint
+
+# imports for printing a matrix, nicely
+pp = pprint.PrettyPrinter()
+
+# complete LPS solution
+
+
 def lps(input_string):
     '''
      In the solution, we are looping over the elements of our `input_string` 
      using two `for` loops; these are each of $O(N)$ and nested this becomes $O(N^2)$. 
      This behavior dominates our optimized solution.
     '''
+    n = len(input_string)
 
-    # TODO: Complete this implementation of the LPS function
-    # The function should return one value: the LPS length for the given input string
-    pass
+    # create a lookup table to store results of subproblems
+    L = [[0 for x in range(n)] for x in range(n)]
+
+    # strings of length 1 have LPS length = 1
+    for i in range(n):
+        L[i][i] = 1
+
+    # consider all substrings
+    for s_size in range(2, n+1):
+        for start_idx in range(n-s_size+1):
+            end_idx = start_idx + s_size - 1
+            if s_size == 2 and input_string[start_idx] == input_string[end_idx]:
+                # match with a substring of length 2
+                L[start_idx][end_idx] = 2
+            elif input_string[start_idx] == input_string[end_idx]:
+                # general match case
+                L[start_idx][end_idx] = L[start_idx+1][end_idx-1] + 2
+            else:
+                # no match case, taking the max of two values
+                L[start_idx][end_idx] = max(
+                    L[start_idx][end_idx-1], L[start_idx+1][end_idx])
+
+    # debug line
+    # pp.pprint(L)
+
+    return L[0][n-1]  # value in top right corner of matrix
 
 
 # TESTS
