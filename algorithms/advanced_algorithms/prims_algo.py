f70b8bb0773e8b7027bc124a3dd4087ba987a231
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-13 19:50:48 -0500

    and  for items with multiple entries

--- algorithms/advanced_algorithms/prims_algo.py
+++ algorithms/advanced_algorithms/prims_algo.py
@@ -3,22 +3,15 @@ import heapq
 # initialize an empty list
 minHeap = list()
 
-# insert 5 into heap
-heapq.heappush(minHeap, 6)
+heapq.heappush(minHeap, (0, 1))
 
-# insert 6 into heap
-heapq.heappush(minHeap, 6)
+heapq.heappush(minHeap, (-1, 5))
 
-# insert 2 into heap
-heapq.heappush(minHeap, 2)
+heapq.heappush(minHeap, (2, 0))
 
-# insert 1 into heap
-heapq.heappush(minHeap, 1)
+heapq.heappush(minHeap, (5, -1))
 
-# insert 9 into heap
-heapq.heappush(minHeap, 9)
-
-print("After pushing, heap looks like: {}".format(minHeap))
+print("After pushing, now heap looks like: {}".format(minHeap))
 
 
 # pop and return smallest element from the heap
