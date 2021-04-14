1ab16d2e4235dda3ba967ed73256cfef19fa2492
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-13 19:33:49 -0500

    added solution to graph_bfs in bsf.py from class

--- algorithms/advanced_algorithms/bfs.py
+++ algorithms/advanced_algorithms/bfs.py
@@ -44,7 +44,19 @@ graph1.add_edge(nodeS, nodeR)
 
 
 def bfs_search(root_node, search_value):
-    pass
+    visited = set()
+    queue = [root_node]
+
+    while len(queue) > 0:
+        current_node = queue.pop(0)
+        visited.add(current_node)
+
+        if current_node.value == search_value:
+            return current_node
+
+        for child in current_node.children:
+            if child not in visited:
+                queue.append(child)
 
 
 # TESTS
