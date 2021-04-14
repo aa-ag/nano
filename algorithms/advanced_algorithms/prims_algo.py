23b1ca310cec089a7ea118a8f72424e9fd67e84e
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-13 19:52:04 -0500

    added main function and test cases for prims_algo.py

--- algorithms/advanced_algorithms/prims_algo.py
+++ algorithms/advanced_algorithms/prims_algo.py
@@ -20,3 +20,51 @@ smallest = heapq.heappop(minHeap)
 print("Smallest element in the original list was: {}".format(smallest))
 
 print("After popping, heap looks like: {}".format(minHeap))
+
+
+def get_minimum_cost_of_connecting(num_islands, bridge_config):
+    """
+    :param: num_islands - number of islands
+    :param: bridge_config - bridge configuration as explained in the problem statement
+    return: cost (int) minimum cost of connecting all islands
+    TODO complete this method to returh minimum cost of connecting all islands
+    """
+    pass
+
+# TESTS
+
+
+def test_function(test_case):
+    num_islands = test_case[0]
+    bridge_config = test_case[1]
+    solution = test_case[2]
+    output = get_minimum_cost_of_connecting(num_islands, bridge_config)
+
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+num_islands = 4
+bridge_config = [[1, 2, 1], [2, 3, 4], [1, 4, 3], [4, 3, 2], [1, 3, 10]]
+solution = 6
+
+test_case = [num_islands, bridge_config, solution]
+test_function(test_case)
+
+
+num_islands = 5
+bridge_config = [[1, 2, 5], [1, 3, 8], [2, 3, 9]]
+solution = 13
+
+test_case = [num_islands, bridge_config, solution]
+test_function(test_case)
+
+
+num_islands = 5
+bridge_config = [[1, 2, 3], [1, 5, 9], [2, 3, 10], [4, 3, 9]]
+solution = 31
+
+test_case = [num_islands, bridge_config, solution]
+test_function(test_case)
