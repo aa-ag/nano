d2db69117184c65656c181d1666b6f35a80eadba
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-13 19:24:27 -0500

    added dfs_recursive.py's solution from class

--- algorithms/advanced_algorithms/dfs_recursive.py
+++ algorithms/advanced_algorithms/dfs_recursive.py
@@ -55,3 +55,42 @@ for each in graph1.nodes:
     for each in each.children:
         print(each.value, end=' ')
     print('\n')
+
+
+'''TO DO'''
+
+
+# Solution
+def dfs_recursion_start(start_node, search_value):
+    # Set to keep track of visited nodes.
+    visited = set()
+    return dfs_recursion(start_node, visited, search_value)
+
+# Recursive function
+
+
+def dfs_recursion(node, visited, search_value):
+    if node.value == search_value:
+        # Don't search in other branches, if found = True
+        found = True
+        return node
+
+    visited.add(node)
+    found = False
+    result = None
+
+    # Conditional recurse on each neighbour
+    for child in node.children:
+        if (child not in visited):
+            result = dfs_recursion(child, visited, search_value)
+
+            # Once the match is found, no more recurse
+            if found:
+                break
+    return result
+
+
+assert nodeA == dfs_recursion_start(nodeG, 'A')
+assert nodeA == dfs_recursion_start(nodeS, 'A')
+assert nodeS == dfs_recursion_start(nodeP, 'S')
+assert nodeR == dfs_recursion_start(nodeH, 'R')
